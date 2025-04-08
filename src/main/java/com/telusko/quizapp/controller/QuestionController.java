33a8148467eef2ecaa2e96a983da3ac885606e30
yogiboh <sabiyogi19@gmail.com>
2025-04-08 12:19:01 -0700

    comment added

--- src/main/java/com/telusko/quizapp/controller/QuestionController.java
+++ src/main/java/com/telusko/quizapp/controller/QuestionController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+//Controller for the code
 @RestController
 @RequestMapping("question")
 public class QuestionController {
